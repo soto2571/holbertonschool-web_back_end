8edf4f4297f62af82d49c666e7faf31c0637b79d
Sebastian <sebastian.soto1649@gmail.com>
2024-08-15 17:09:06 -0400

    Auto commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" 
+This module would have a function that concatinate two strings
+"""
+
+
+def concat(a: str,b: str):
+    """ 
+    It would concatinate two strings
+
+    Parameter:
+    a: first string
+    b: second string
+
+    Return: The two strings concatinated
+    """
+    return a + b
