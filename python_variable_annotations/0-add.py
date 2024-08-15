733562d00934414d9b5337cf15f7086f854e0db7
Sebastian <sebastian.soto1649@gmail.com>
2024-08-15 16:52:21 -0400

    Auto commit

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This module contain a function that return the sum of two float number
+"""
+
+def add(a, b):
+    """
+    This function will return the sum of two float numbers
+
+    Parameters:
+    a (float): First float number
+    b (float): Second float number
+
+    Returns:
+    float: The sum of the two float number.
+    """
+    a + b
