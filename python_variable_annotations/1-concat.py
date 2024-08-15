0d08e3a47a58775a0fa41889f9990f1eff7a994d
Sebastian <sebastian.soto1649@gmail.com>
2024-08-15 17:11:33 -0400

    Auto commit

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" 
+"""
 This module would have a function that concatinate two strings
 """
 
 
-def concat(a: str,b: str) -> str:
-    """ 
+def concat(a: str, b: str) -> str:
+    """
     It would concatinate two strings
 
     Parameter:
