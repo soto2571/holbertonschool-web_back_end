cdd55f7a20afe1ef441a812597102393b6be6660
Sebastian <sebastian.soto1649@gmail.com>
2024-08-21 16:47:15 -0400

    Auto commit

---
+++ python_async_function/python-async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async generator task 0"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """function waits 1 second then yield a random number(0, 10)"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        
